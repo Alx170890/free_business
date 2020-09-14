6c0389f06cb0901377b1719322c14c383983b533
Alexey <Alx170890@gmail.com>
2020-09-14 10:30:11 +0300

    Initial free_business

---
+++ js/index.js
@@ -0,0 +1,15 @@
+// (function slider() {               
+//     $(".header__content-slider").slick({		        
+//     //   vertical: true,
+//       slidesToShow: 1,
+//       slidesToScroll: 1,
+//       verticalSwiping: false,		        
+//       infinite: false,		        
+//       draggable: false,
+//       arrows: false, 
+//       autoplay:	true,
+//       autoplaySpeed: 3000,
+//       adaptiveHeigh: true,  
+//       dots:	true
+//     });
+// }());
